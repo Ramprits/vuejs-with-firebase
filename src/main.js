4442ded71745797ff4504f001ed33338a09e7a82
Ramprit Sahani <21260321+Ramprits@users.noreply.github.com>
2021-05-15 10:23:21 +0530

    Adding routing

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import router from "./routes";
 import store from "./store";
 import Title from "./components/Title.vue";
-
 const app = createApp(App);
 app.use(router).use(store).mount("#app");
 app.component("vue-title", Title);
